abe114c4da48a3c74f3e376493c079c09695e8eb
zl <151681925@qq.com>
2021-02-26 13:40:19 +0800

    水环境动态监测初始化项目

---
+++ monitor/src/main/java/com/pd/wxbase/MonitorApplication.java
@@ -0,0 +1,30 @@
+package com.pd.wxbase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+@EnableEurekaClient
+@EnableScheduling
+@ComponentScan("com.pd")
+public class MonitorApplication {
+
+	private  static  final Logger LOG = LoggerFactory.getLogger(MonitorApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(MonitorApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Wvbase地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
+
+	}
+
+}
---
+++ server/src/main/java/com/pd/server/main/dto/basewx/my/WxyyYyslGuoupDto.java
@@ -0,0 +1,122 @@
+package com.pd.server.main.dto.basewx.my;
+
+public class WxyyYyslGuoupDto {
+
+    private String deptcode;
+    private Integer curNfYysl;
+    private Integer beforeNfYysl;
+    private Integer curYfYysl;
+    private Integer beforeYfYysl;
+    private String tbzzYysl;//同比增长
+    private String hbzzYysl;//环比增长
+    private Integer curNfDcsl;
+    private Integer beforeNfDcsl;
+    private Integer curYfDcsl;
+    private Integer beforeYfDcsl;
+    private String tbzzDcsl;
+    private String hbzzDcsl;
+
+    public String getDeptcode() {
+        return deptcode;
+    }
+
+    public void setDeptcode(String deptcode) {
+        this.deptcode = deptcode;
+    }
+
+    public Integer getCurNfYysl() {
+        return curNfYysl;
+    }
+
+    public void setCurNfYysl(Integer curNfYysl) {
+        this.curNfYysl = curNfYysl;
+    }
+
+    public Integer getBeforeNfYysl() {
+        return beforeNfYysl;
+    }
+
+    public void setBeforeNfYysl(Integer beforeNfYysl) {
+        this.beforeNfYysl = beforeNfYysl;
+    }
+
+    public Integer getCurYfYysl() {
+        return curYfYysl;
+    }
+
+    public void setCurYfYysl(Integer curYfYysl) {
+        this.curYfYysl = curYfYysl;
+    }
+
+    public Integer getBeforeYfYysl() {
+        return beforeYfYysl;
+    }
+
+    public void setBeforeYfYysl(Integer beforeYfYysl) {
+        this.beforeYfYysl = beforeYfYysl;
+    }
+
+    public String getTbzzYysl() {
+        return tbzzYysl;
+    }
+
+    public void setTbzzYysl(String tbzzYysl) {
+        this.tbzzYysl = tbzzYysl;
+    }
+
+    public String getHbzzYysl() {
+        return hbzzYysl;
+    }
+
+    public void setHbzzYysl(String hbzzYysl) {
+        this.hbzzYysl = hbzzYysl;
+    }
+
+    public Integer getCurNfDcsl() {
+        return curNfDcsl;
+    }
+
+    public void setCurNfDcsl(Integer curNfDcsl) {
+        this.curNfDcsl = curNfDcsl;
+    }
+
+    public Integer getBeforeNfDcsl() {
+        return beforeNfDcsl;
+    }
+
+    public void setBeforeNfDcsl(Integer beforeNfDcsl) {
+        this.beforeNfDcsl = beforeNfDcsl;
+    }
+
+    public Integer getCurYfDcsl() {
+        return curYfDcsl;
+    }
+
+    public void setCurYfDcsl(Integer curYfDcsl) {
+        this.curYfDcsl = curYfDcsl;
+    }
+
+    public Integer getBeforeYfDcsl() {
+        return beforeYfDcsl;
+    }
+
+    public void setBeforeYfDcsl(Integer beforeYfDcsl) {
+        this.beforeYfDcsl = beforeYfDcsl;
+    }
+
+    public String getTbzzDcsl() {
+        return tbzzDcsl;
+    }
+
+    public void setTbzzDcsl(String tbzzDcsl) {
+        this.tbzzDcsl = tbzzDcsl;
+    }
+
+    public String getHbzzDcsl() {
+        return hbzzDcsl;
+    }
+
+    public void setHbzzDcsl(String hbzzDcsl) {
+        this.hbzzDcsl = hbzzDcsl;
+    }
+}
