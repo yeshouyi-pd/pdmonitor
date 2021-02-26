e71d7fea50882a23e24c4fe45115ddedf7de7a98
ltj5200 <ltj4035389>
2021-02-26 09:54:32 +0800

    1

--- eureka/src/main/java/com/pd/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/pd/eureka/EurekaApplication.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.Environment;
 public class EurekaApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
 
+
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(EurekaApplication.class);
 		Environment env = app.run(args).getEnvironment();
