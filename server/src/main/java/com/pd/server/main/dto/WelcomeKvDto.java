e6679d88ff5a866cd424e2b62eb14c21ffbed8e1
ltj5200 <ltj4035389>
2021-05-08 08:49:30 +0800

    首页布局

---
+++ server/src/main/java/com/pd/server/main/dto/WelcomeKvDto.java
@@ -0,0 +1,14 @@
+package com.pd.server.main.dto;
+
+public class WelcomeKvDto  extends  KvMapDto{
+
+    private  String deptname ;
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname;
+    }
+}
