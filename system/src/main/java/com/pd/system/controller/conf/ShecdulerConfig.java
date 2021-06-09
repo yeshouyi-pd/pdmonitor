d6d648587d9e4f73a3ec2130101fc45f1af0bdae
xumin-0526 <18827512017@189.cn>
2021-06-09 15:51:45 +0800

    1

---
+++ system/src/main/java/com/pd/system/controller/conf/ShecdulerConfig.java
@@ -0,0 +1,18 @@
+package com.pd.system.controller.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+@Configuration//用线程池给不同定时任务分配不同的线程
+public class ShecdulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        //设定一个长度10的定时任务线程池
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(10));
+    }
+
+}
