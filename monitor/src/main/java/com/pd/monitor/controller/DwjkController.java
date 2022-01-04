53fda9d1238c03c4cef3f5e1aa6ca85fd95fde0b
xumin-0526 <18827512017@189.cn>
2022-01-04 16:45:36 +0800

    不知道时间为什么会有问题

--- monitor/src/main/java/com/pd/monitor/controller/DwjkController.java
+++ monitor/src/main/java/com/pd/monitor/controller/DwjkController.java
@@ -3,12 +3,14 @@ package com.pd.monitor.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.pd.monitor.wx.conf.BaseWxController;
 import com.pd.server.main.domain.*;
+import com.pd.server.main.dto.EquipmentFileDto;
 import com.pd.server.main.dto.InterfaceLogDto;
 import com.pd.server.main.dto.ResponseDto;
 import com.pd.server.main.service.AttrService;
 import com.pd.server.main.service.AuthorizeInfoService;
 import com.pd.server.main.service.EquipmentFileService;
 import com.pd.server.main.service.InterfaceLogService;
+import com.pd.server.util.CopyUtil;
 import com.pd.server.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -138,8 +140,9 @@ public class DwjkController extends BaseWxController {
             fileCa.andSbbhEqualTo(qqcs.get("sbsn").toString());
             fileCa.andRqEqualTo(qqcs.get("cjrq").toString());
             List<EquipmentFile> equipmentFiles = equipmentFileService.listAll(fileExample);
+            List<EquipmentFileDto> equipmentFilesDto = CopyUtil.copyList(equipmentFiles,EquipmentFileDto.class);
             responseDto.setCode("0000");
-            responseDto.setContent(equipmentFiles);
+            responseDto.setContent(equipmentFilesDto);
             interfaceLog.setFhsj(JSONObject.toJSONString(responseDto));
             interfaceLogService.save(interfaceLog);
         }
