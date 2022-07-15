22d191456c18ce08ea3d1a76720c9026e10aa492
xumin-0526 <18827512017@189.cn>
2022-07-15 09:20:24 +0800

    修改报警文件显示

--- admin/src/views/monitor/equipmentFile.vue
+++ admin/src/views/monitor/equipmentFile.vue
@@ -66,18 +66,19 @@
 
     <div>
       <div style="display: flex;flex-wrap: wrap;margin-bottom: 30px;">
-        <div v-for="(item,index) in equipmentFiles" style="margin:20px;width: 150px;height: 280px;text-align: center;">
-          <div style="text-align: center;width: 100px;margin: 0 auto;">
-            <img :src="item.tplj" style="width: 100px;height: 200px;cursor: pointer;" v-on:click="checkImg(item,index)">
+        <div v-for="(item,index) in equipmentFiles" style="margin:20px;width: 150px;height: 300px;text-align: center;">
+          <div style="text-align: center;width: 100px;margin: 0 auto;" v-if="item.hasTxt">
+            <img v-if="item.hasTxt" :src="item.txtSrc" style="width: 100px;height: 200px;cursor: pointer;" v-on:click="checkImg(item,index)">
           </div>
           <div style="margin: 0 auto;">{{waterEquipments|optionNSArray(item.sbbh)}}</div>
           <div style="margin: 0 auto;">{{item.cjsj}}</div>
+          <div style="margin: 0 auto;word-wrap: break-word;">{{item.tplj.substring(item.tplj.lastIndexOf("/")+1,item.tplj.length)}}</div>
           <div style="margin: 0 auto;" v-if="item.hasAudio">
             <button class="btn btn-white btn-default btn-round" style="margin: 0 auto;" v-on:click="downloadAudio(item)">
               <i class="ace-icon fa fa-volume-down red2">下载音频</i>
             </button>
           </div>
-          <div style="margin: 0 auto;" v-if="item.hasTxt">
+          <div style="margin: 0 auto;">
             <button class="btn btn-white btn-default btn-round" style="margin: 0 auto;" v-on:click="downloadTxt(item)">
               <i class="ace-icon fa fa-volume-down red2">发现头数</i>
             </button>
@@ -102,6 +103,7 @@
               </button>
               <span style="font-size: 18px;margin: 10px 20px;">设备名称：{{waterEquipments|optionNSArray(curSbsn)}}</span>
               <span style="font-size: 18px;">采集时间：{{curCjsj}}</span>
+              <span style="font-size: 18px;">文件名称：{{curWjmc.substring(curWjmc.lastIndexOf("/")+1,curWjmc.length)}}</span>
             </div>
             <div style="display: flex;">
               <div style="text-align: left;margin: auto;">
@@ -173,6 +175,7 @@ export default {
       curTplj:'',
       curSbsn:'',
       curCjsj:'',
+      curWjmc:'',
       waterEquipments:[],
     }
   },
@@ -206,17 +209,19 @@ export default {
     beforePic(){
       let _this = this;
       _this.curIndex = _this.curIndex-1;
-      _this.curTplj = _this.equipmentFiles[_this.curIndex].tplj;
+      _this.curTplj = _this.equipmentFiles[_this.curIndex].txtSrc;
       _this.curSbsn = _this.equipmentFiles[_this.curIndex].sbbh;
       _this.curCjsj = _this.equipmentFiles[_this.curIndex].cjsj;
+      _this.curWjmc = _this.equipmentFiles[_this.curIndex].tplj;
       _this.$forceUpdate();
     },
     nextPic(){
       let _this = this;
       _this.curIndex = _this.curIndex+1;
-      _this.curTplj = _this.equipmentFiles[_this.curIndex].tplj;
+      _this.curTplj = _this.equipmentFiles[_this.curIndex].txtSrc;
       _this.curSbsn = _this.equipmentFiles[_this.curIndex].sbbh;
       _this.curCjsj = _this.equipmentFiles[_this.curIndex].cjsj;
+      _this.curWjmc = _this.equipmentFiles[_this.curIndex].tplj;
       _this.$forceUpdate();
     },
     /**
@@ -272,9 +277,10 @@ export default {
     checkImg(item,index){
       let _this = this;
       _this.curIndex = index;
-      _this.curTplj = item.tplj;
+      _this.curTplj = item.txtSrc;
       _this.curSbsn = item.sbbh;
       _this.curCjsj = item.cjsj;
+      _this.curWjmc = item.tplj;
       _this.equipmentFile = $.extend({}, item);
       _this.$forceUpdate();
       $("#img-modal").modal("show");
@@ -296,9 +302,10 @@ export default {
       window.location.href = url;
     },
     downloadTxt(obj){
-      console.log(obj.txtSrc.substring(obj.txtSrc.lastIndexOf("/")+1,obj.txtSrc.length));
+      //console.log(obj.txtSrc.substring(obj.txtSrc.lastIndexOf("/")+1,obj.txtSrc.length));
       let _this = this;
-      let paramsStr = "fileUrl="+obj.tplj.substring(0,obj.tplj.lastIndexOf("/")+1)+"&fileName="+obj.txtSrc.substring(obj.txtSrc.lastIndexOf("/")+1,obj.txtSrc.length);
+      //let paramsStr = "fileUrl="+obj.tplj.substring(0,obj.tplj.lastIndexOf("/")+1)+"&fileName="+obj.txtSrc.substring(obj.txtSrc.lastIndexOf("/")+1,obj.txtSrc.length);
+      let paramsStr = "fileUrl="+obj.tplj.substring(0,obj.tplj.lastIndexOf("/")+1)+"&fileName="+obj.tplj.substring(obj.tplj.lastIndexOf("/")+1,obj.tplj.length);
       let url = process.env.VUE_APP_SERVER + '/monitor/download/audio/downAudioFile?'+paramsStr;
       console.log(url);
       window.location.href = url;
