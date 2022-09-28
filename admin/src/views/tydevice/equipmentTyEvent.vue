2d5510e1bc9929a2a516efc1edacbdd9e8a69cf6
xumin-0526 <18827512017@189.cn>
2022-09-28 13:12:46 +0800

    修改

--- admin/src/views/tydevice/equipmentTyEvent.vue
+++ admin/src/views/tydevice/equipmentTyEvent.vue
@@ -161,7 +161,7 @@ export default {
       _this.$ajax.get(url).then((response)=>{
         if(response.data && response.data.message && response.data.message.includes("系统异常")){
           Toast.error("系统异常，请联系管理员！");
-        }else if(response.data && response.data.includes("未找到对应视频")){
+        }else if(response.data && response.data.includes("未找到")){
           Toast.error("未找到对应视频！");
         }else{
           window.location.href = url;
