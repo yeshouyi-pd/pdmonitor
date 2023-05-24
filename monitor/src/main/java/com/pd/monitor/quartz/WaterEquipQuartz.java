921790a0c55c00fa28329cb272656e8ad911142d
xumin-0526 <18827512017@189.cn>
2023-05-24 09:49:25 +0800

    修改

--- monitor/src/main/java/com/pd/monitor/quartz/WaterEquipQuartz.java
+++ monitor/src/main/java/com/pd/monitor/quartz/WaterEquipQuartz.java
@@ -39,7 +39,7 @@ public class WaterEquipQuartz {
     /* 每天凌晨5点50打开设备通道一 */
     @Scheduled(cron = "0 50 5 * * ? ")
     public void openLoop(){
-        String iccids = attrService.findByAttrKey("iccids");
+        String iccids = attrService.findByAttrKey("iccidsZsTd1");
         String zszxml = attrService.findByAttrKey("zszxml");//早上5点50执行的命令
         saveTaskList(iccids, zszxml);
     }
@@ -47,7 +47,7 @@ public class WaterEquipQuartz {
     /* 每天晚上7点10关闭设备通道一 */
     @Scheduled(cron = "0 10 19 * * ? ")
     public void closeLoop(){
-        String iccids = attrService.findByAttrKey("iccids");
+        String iccids = attrService.findByAttrKey("iccidsWsTd1");
         String wszxml = attrService.findByAttrKey("wszxml");//晚上7点10执行的命令
         saveTaskList(iccids, wszxml);
     }
@@ -55,7 +55,7 @@ public class WaterEquipQuartz {
     /* 每天晚上5点40打开设备通道二 */
     @Scheduled(cron = "0 40 17 * * ? ")
     public void open2Loop(){
-        String iccids = attrService.findByAttrKey("iccids2");
+        String iccids = attrService.findByAttrKey("iccidsZsTd2");
         String zszxml = attrService.findByAttrKey("zszxml2");//晚上5点40执行的命令
         saveTaskList(iccids, zszxml);
     }
@@ -63,7 +63,7 @@ public class WaterEquipQuartz {
     /* 每天晚上7点10关闭设备通道二 */
     @Scheduled(cron = "0 10 19 * * ? ")
     public void close2Loop(){
-        String iccids = attrService.findByAttrKey("iccids2");
+        String iccids = attrService.findByAttrKey("iccidsWsTd2");
         String wszxml = attrService.findByAttrKey("wszxml2");//晚上7点10执行的命令
         saveTaskList(iccids, wszxml);
     }
