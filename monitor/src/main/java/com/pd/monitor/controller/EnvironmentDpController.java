abf7524e7498ec85216bbe1d59ef937d309b2bda
xumin-0526 <18827512017@189.cn>
2023-11-08 16:03:27 +0800

    修改

--- monitor/src/main/java/com/pd/monitor/controller/EnvironmentDpController.java
+++ monitor/src/main/java/com/pd/monitor/controller/EnvironmentDpController.java
@@ -1,16 +1,13 @@
 package com.pd.monitor.controller;
 
 import com.pd.server.main.domain.*;
-import com.pd.server.main.dto.ResponseDto;
+import com.pd.server.main.dto.*;
 import com.pd.server.main.service.*;
 import com.pd.server.util.DateUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -35,82 +32,97 @@ public class EnvironmentDpController {
     @Resource
     private WaveDataService waveDataService;
 
-    @PostMapping("/getWaveDataTodayData/{sbbh}")
-    public ResponseDto getWaveDataTodayData(@PathVariable String sbbh){
+    @PostMapping("/getWaveDataData")
+    public ResponseDto getWaveDataData(@RequestBody WaveDataDto waveDataDto){
         ResponseDto responseDto = new ResponseDto();
         WaveDataExample example = new WaveDataExample();
         WaveDataExample.Criteria ca = example.createCriteria();
-        if(!StringUtils.isEmpty(sbbh)){
-            ca.andSbbhEqualTo(sbbh);
+        if(!StringUtils.isEmpty(waveDataDto.getSbbh())){
+            ca.andSbbhEqualTo(waveDataDto.getSbbh());
         }
-        //ca.andCjsjEqualTo("2023-10-01","%Y-%m-%d");
-        ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        if(!StringUtils.isEmpty(waveDataDto.getStime())){
+            ca.andCjsjEqualTo(waveDataDto.getStime(),"%Y-%m-%d");
+        }else{
+            ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        }
         example.setOrderByClause(" cjsj ");
         List<WaveData> lists = waveDataService.selectByExample(example);
         responseDto.setContent(lists);
         return responseDto;
     }
 
-    @PostMapping("/getWaterQualityNewTodayData/{sbbh}")
-    public ResponseDto getWaterQualityNewTodayData(@PathVariable String sbbh){
+    @PostMapping("/getWaterQualityNewData")
+    public ResponseDto getWaterQualityNewData(@RequestBody WaterQualityNewDto waterQualityNewDto){
         ResponseDto responseDto = new ResponseDto();
         WaterQualityNewExample example = new WaterQualityNewExample();
         WaterQualityNewExample.Criteria ca = example.createCriteria();
-        if(!StringUtils.isEmpty(sbbh)){
-            ca.andSbbhEqualTo(sbbh);
+        if(!StringUtils.isEmpty(waterQualityNewDto.getSbbh())){
+            ca.andSbbhEqualTo(waterQualityNewDto.getSbbh());
         }
-        //ca.andCjsjEqualTo("2023-10-20","%Y-%m-%d");
-        ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        if(!StringUtils.isEmpty(waterQualityNewDto.getStime())){
+            ca.andCjsjEqualTo(waterQualityNewDto.getStime(),"%Y-%m-%d");
+        }else{
+            ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        }
         example.setOrderByClause(" cjsj ");
         List<WaterQualityNew> lists = waterQualityNewService.selectByExample(example);
         responseDto.setContent(lists);
         return responseDto;
     }
 
-    @PostMapping("/getMeteorologicalTodayData/{sbbh}")
-    public ResponseDto getMeteorologicalTodayData(@PathVariable String sbbh){
+    @PostMapping("/getMeteorologicalData")
+    public ResponseDto getMeteorologicalData(@RequestBody MeteorologicalDataDto meteorologicalDataDto){
         ResponseDto responseDto = new ResponseDto();
         MeteorologicalDataExample example = new MeteorologicalDataExample();
         MeteorologicalDataExample.Criteria ca = example.createCriteria();
-        if(!StringUtils.isEmpty(sbbh)){
-            ca.andBzEqualTo(sbbh);
+        if(!StringUtils.isEmpty(meteorologicalDataDto.getBz())){
+            ca.andBzEqualTo(meteorologicalDataDto.getBz());
         }
-        //ca.andCjsjEqualTo("2023-10-01","%Y-%m-%d");
-        ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        if(!StringUtils.isEmpty(meteorologicalDataDto.getStime())){
+            ca.andCjsjEqualTo(meteorologicalDataDto.getStime(),"%Y-%m-%d");
+        }else{
+            ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        }
         example.setOrderByClause(" cjsj ");
         List<MeteorologicalData> lists = meteorologicalDataService.selectByExample(example);
         responseDto.setContent(lists);
         return responseDto;
     }
 
-    @PostMapping("/getTurbidityTodayData/{sbbh}")
-    public ResponseDto getTurbidityTodayData(@PathVariable String sbbh){
+    @PostMapping("/getTurbidityData")
+    public ResponseDto getTurbidityData(@RequestBody TurbidityDto turbidityDto){
         ResponseDto responseDto = new ResponseDto();
         TurbidityExample turbidityExample = new TurbidityExample();
         TurbidityExample.Criteria ca = turbidityExample.createCriteria();
-        if(!StringUtils.isEmpty(sbbh)){
-            ca.andBzEqualTo(sbbh);
+        if(!StringUtils.isEmpty(turbidityDto.getBz())){
+            ca.andBzEqualTo(turbidityDto.getBz());
         }
-        //ca.andDateTimeEqualTo("2023-10-01","%Y-%m-%d");
-        ca.andDateTimeEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        if(!StringUtils.isEmpty(turbidityDto.getStime())){
+            ca.andDateTimeEqualTo(turbidityDto.getStime(),"%Y-%m-%d");
+        }else{
+            ca.andDateTimeEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        }
         turbidityExample.setOrderByClause(" date_time ");
         List<Turbidity> lists = turbidityService.selectByExample(turbidityExample);
         responseDto.setContent(lists);
         return responseDto;
     }
 
-    @PostMapping("/getCurrentMeterTodayData/{sbbh}")
-    public ResponseDto getCurrentMeterTodayData(@PathVariable String sbbh){
+    @PostMapping("/getCurrentMeterData")
+    public ResponseDto getCurrentMeterData(@RequestBody CurrentMeterDto currentMeterDto){
         ResponseDto responseDto = new ResponseDto();
         CurrentMeter currentMeter = new CurrentMeter();
         CurrentMeterExample currentMeterExample = new CurrentMeterExample();
         CurrentMeterExample.Criteria ca = currentMeterExample.createCriteria();
-        if(!StringUtils.isEmpty(sbbh)){
-            ca.andBzEqualTo(sbbh);
+        if(!StringUtils.isEmpty(currentMeterDto.getBz())){
+            ca.andBzEqualTo(currentMeterDto.getBz());
         }
-        //ca.andCjsjEqualTo("2023-09-26","%Y-%m-%d");
-        ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
-        currentMeterExample.setOrderByClause(" cjsj ");
+        if(!StringUtils.isEmpty(currentMeterDto.getStime())){
+            ca.andCjsjEqualTo(currentMeterDto.getStime(),"%Y-%m-%d");
+        }else{
+            ca.andCjsjEqualTo(DateUtils.getDateToStrFormat(new Date(),"yyyy-MM-dd"),"%Y-%m-%d");
+        }
+        currentMeterExample.setOrderByClause(" cjsj desc ");
         List<CurrentMeter> currentMeterList = currentMeterService.selectByExample(currentMeterExample);
         if(currentMeterList.size()>0){
             currentMeter = currentMeterList.get(0);
