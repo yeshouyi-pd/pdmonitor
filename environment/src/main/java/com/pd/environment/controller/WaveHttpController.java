f04342232d84f49e8fb166c6e7e6cbcd1dd0803e
xumin-0526 <18827512017@189.cn>
2023-12-07 17:22:47 +0800

    第三方环境数据对接接口

---
+++ environment/src/main/java/com/pd/environment/controller/WaveHttpController.java
@@ -0,0 +1,76 @@
+package com.pd.environment.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.pd.server.main.domain.WaterEquipmentExample;
+import com.pd.server.main.dto.ResponseDto;
+import com.pd.server.main.dto.WaveDataDto;
+import com.pd.server.main.service.WaterEquipmentService;
+import com.pd.server.main.service.WaveDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/wave")
+public class WaveHttpController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WaveHttpController.class);
+    public static final String BUSINESS_NAME = "接收第三方波浪数据";
+
+    @Resource
+    private WaveDataService waveDataService;
+    @Resource
+    private WaterEquipmentService waterEquipmentService;
+
+    /**
+     * 保存数据
+     */
+    @PostMapping("/saveData")
+    public ResponseDto saveData(@RequestBody JSONObject jsonObject) {
+        ResponseDto responseDto = new ResponseDto();
+        try {
+            if(StringUtils.isEmpty(jsonObject.get("waveH")) || StringUtils.isEmpty(jsonObject.get("waveDirection"))
+                    || StringUtils.isEmpty(jsonObject.get("wavePeriod")) || StringUtils.isEmpty(jsonObject.get("sbbh"))
+                    || StringUtils.isEmpty(jsonObject.get("cjsj")) ){
+                responseDto.setSuccess(false);
+                responseDto.setMessage("参数错误");
+                return responseDto;
+            }
+            String cjsj = jsonObject.getString("cjsj");
+            String isFlag = "^(\\d{4})-(\\d{2})-(\\d{2})\\s+([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$";//判断时间
+            if(!cjsj.matches(isFlag)){
+                responseDto.setSuccess(false);
+                responseDto.setMessage("时间格式错误");
+                return responseDto;
+            }
+            WaterEquipmentExample example = new WaterEquipmentExample();
+            List<String> sbbhs= waterEquipmentService.findSbbh(example);
+            if(!sbbhs.contains(jsonObject.getString("sbbh"))){
+                responseDto.setSuccess(false);
+                responseDto.setMessage("设备编号错误");
+                return responseDto;
+            }
+            WaveDataDto dto = new WaveDataDto();
+            dto.setCjsj(jsonObject.getDate("cjsj"));
+            dto.setSbbh(jsonObject.getString("sbbh"));
+            dto.setWaveH(jsonObject.getString("waveH"));
+            dto.setWaveDirection(jsonObject.getString("waveDirection"));
+            dto.setWavePeriod(jsonObject.getString("wavePeriod"));
+            waveDataService.save(dto);
+            responseDto.setSuccess(true);
+            responseDto.setMessage("保存成功");
+        }catch (Exception e){
+            LOG.error("接收到的数据："+JSONObject.toJSONString(jsonObject)+"====错误原因："+e.getMessage());
+            responseDto.setMessage(e.getMessage());
+            responseDto.setSuccess(false);
+        }
+        return responseDto;
+    }
+}
