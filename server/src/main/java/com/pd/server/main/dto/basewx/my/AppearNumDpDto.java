e1371cec520823ffb36127c7bbbaad0749ea7eb2
xumin-0526 <18827512017@189.cn>
2023-12-12 10:55:24 +0800

    修改大屏

---
+++ server/src/main/java/com/pd/server/main/dto/basewx/my/AppearNumDpDto.java
@@ -0,0 +1,24 @@
+package com.pd.server.main.dto.basewx.my;
+
+public class AppearNumDpDto {
+
+    private Integer sumAlarmNum;
+
+    private String xs;
+
+    public Integer getSumAlarmNum() {
+        return sumAlarmNum;
+    }
+
+    public void setSumAlarmNum(Integer sumAlarmNum) {
+        this.sumAlarmNum = sumAlarmNum;
+    }
+
+    public String getXs() {
+        return xs;
+    }
+
+    public void setXs(String xs) {
+        this.xs = xs;
+    }
+}
