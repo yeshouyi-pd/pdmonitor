6226eb680831fe190732fed296925b31880e1ca2
xumin-0526 <18827512017@189.cn>
2024-01-03 17:09:03 +0800

    设备超过5台不在线

--- monitor/src/main/java/com/pd/monitor/quartz/DeviceStateLogQuartz.java
+++ monitor/src/main/java/com/pd/monitor/quartz/DeviceStateLogQuartz.java
@@ -1,21 +1,27 @@
 package com.pd.monitor.quartz;
 
+import com.pd.monitor.utils.SendSmsTool;
+import com.pd.monitor.wx.conf.WxRedisConfig;
 import com.pd.server.main.domain.WaterEquiplog;
 import com.pd.server.main.domain.WaterEquiplogExample;
+import com.pd.server.main.domain.WaterEquipment;
 import com.pd.server.main.domain.WaterEquipmentExample;
 import com.pd.server.main.dto.DeviceStateLogDto;
 import com.pd.server.main.service.DeviceStateLogService;
 import com.pd.server.main.service.WaterEquiplogService;
+import com.pd.server.main.service.WaterEquipmentService;
 import com.pd.server.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @EnableScheduling
@@ -27,6 +33,8 @@ public class DeviceStateLogQuartz {
     private DeviceStateLogService deviceStateLogService;
     @Resource
     private WaterEquiplogService waterEquiplogService;
+    @Resource
+    private WaterEquipmentService waterEquipmentService;
 
     //每天晚上11点30执行
     @Scheduled(cron = "0 30 23 * * ? ")
@@ -48,6 +56,23 @@ public class DeviceStateLogQuartz {
             deviceStateLogService.save(deviceStateLogDto);
         }
         waterEquiplogList.clear();
+    }
+
+    //每天晚上11点30执行
+    @Scheduled(cron = "0 0 08,12,18 * * ? ")
+    public void sendMsg(){
+        Map<String, String> attrMap = WxRedisConfig.getAttrMap();
+        if(!StringUtils.isEmpty(attrMap.get("deviceErrorPhone"))){
+            WaterEquipmentExample example = new WaterEquipmentExample();
+            WaterEquipmentExample.Criteria ca = example.createCriteria();
+            ca.andSblbEqualTo("0001");
+            ca.andSbztEqualTo("2");
+            List<WaterEquipment> lists = waterEquipmentService.list(example);
+            if(lists.size()>5){
+                SendSmsTool.sendSms("2038983","",attrMap.get("deviceErrorPhone"));
+            }
+            lists.clear();
+        }
     }
 
 //    @Scheduled(cron = "0 */3 * * * ? ")
