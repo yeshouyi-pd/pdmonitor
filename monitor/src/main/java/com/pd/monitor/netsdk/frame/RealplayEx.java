3e84acfdfa7598bb9d638638d8f6a42fc7dbafb9
ysy <172793208@qq.com>
2024-04-12 18:09:41 +0800

    升级JAN版本  修改gatewat websocket字节数

--- monitor/src/main/java/com/pd/monitor/netsdk/frame/RealplayEx.java
+++ monitor/src/main/java/com/pd/monitor/netsdk/frame/RealplayEx.java
@@ -175,15 +175,16 @@ public class RealplayEx {
                            int dwBufSize, int param, Pointer dwUser) {
             try {
                 //实际可为通道号
-                System.out.println("重内存指针获取4字节int值"+dwUser.getInt(0));
+              // System.out.println("重内存指针获取4字节int值"+dwUser.getInt(0));
                 int channel = dwUser.getInt(0);
-                System.out.println("C++的内存指针"+Pointer.nativeValue(dwUser));
+
+              //  System.out.println("C++的内存指针"+Pointer.nativeValue(dwUser));
                 //将内容转换为字节数组
                 byte[] bytes = pBuffer.getByteArray(0, dwBufSize);
                 if ((dwDataType - 1000) == 5) {//回调格式为flv的流
                     server = SpringUtil.getBean(WebSocketServerDh.class);
                     ByteBuffer buffer = ByteBuffer.wrap(bytes);
-                    server.sendBuffer(buffer, channel);
+                    server.sendBuffer(buffer, channel ,lRealHandle);
                 }
             }catch (Exception e){
                 throw e;
--- monitor/src/main/java/com/pd/monitor/netsdk/websocketServer/WebSocketServerDh.java
+++ monitor/src/main/java/com/pd/monitor/netsdk/websocketServer/WebSocketServerDh.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.pd.monitor.netsdk.lib.NetSDKLib;
 import com.pd.monitor.netsdk.module.LoginModule;
 import com.pd.monitor.netsdk.module.OperationModule;
+import com.pd.monitor.netsdk.module.RealPlayModule;
 import com.pd.monitor.netsdk.po.RealPlayInfo;
 import com.pd.monitor.netsdk.service.VideoMonitorService;
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -27,7 +29,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author xutongxue
  * @date 2023/8/23 13:36
  */
-@ServerEndpoint("/device/monitor/{device}/{channel}/{sid}")
+@ServerEndpoint("/device/monitor/{channel}")
 @Component
 public class WebSocketServerDh {
     private static ConfigurableApplicationContext applicationContext;
@@ -39,7 +41,6 @@ public class WebSocketServerDh {
     /**
      * 与某个客户端的连接会话，需要通过它来给客户端发送数据
      */
-    public static Session session;
 
     private static WebSocketServerDh instance;
 
@@ -51,12 +52,12 @@ public class WebSocketServerDh {
     /**
      * 存放每个客户端对应的WebSocket对象,根据设备realPlayHandler建立session
      */
-    public static ConcurrentHashMap<String, Session> sessions = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap< Session ,Integer > sessions = new ConcurrentHashMap<>();
 
     /**
-     * 实时预览句柄-通道
+     * 关联通道号和播放句柄
      */
-    public static ConcurrentHashMap<Long, Integer> lRealHandles = new ConcurrentHashMap<>();
+    public static HashMap<Integer, NetSDKLib.LLong> channlAndlRealHandles = new HashMap<>();
     /**
      * 保存覆盖播放标识
      */
@@ -64,68 +65,49 @@ public class WebSocketServerDh {
 
     private static final Logger log = LoggerFactory.getLogger(WebSocketServerDh.class);
 
-    public static String sid;
 
     /**
      * 有websocket client连接
-     * @param device 设备ID
      * @param channel 预览句柄
      * @param session
      */
     @OnOpen
-    public void OnOpen(@PathParam("device") String device, @PathParam("channel") String channel, @PathParam("sid") String sid, Session session)  throws InterruptedException {
-        log.info("websocket连接进入"+sid);
-        this.session = session; // 保存客户端连接的Session对象
-        this.sid = sid;
-        //设备ID+通道组成唯一性标识
-        String uuid = sid+"-"+channel;
-        if (sessions.containsKey(uuid)) {
-            sessions.put(uuid, session);
-        } else {
-            sessions.put(uuid, session);
-            addOnlineCount();
-        }
-        log.info("websocket connect.session: " + session);
+    public void OnOpen( @PathParam("channel") Integer channel, Session session)  throws InterruptedException {
+
+        sessions.put( session  ,channel);
+        addOnlineCount();
+        log.info("websocket connect.session: " + sessions.size());
     }
 
     /**
      * 连接关闭调用的方法
-     * @param device 设备ID
      * @param channel 预览句柄
      * @param session websocket连接对象
      */
     @OnClose
-    public void onClose(@PathParam("device") String device, @PathParam("channel") String channel, @PathParam("sid") String sid, Session session){
-        System.out.println("websocket 断开" + sid);
-        subOnlineCount();
-        log.error("有一连接关闭！当前在线人数为"+getOnlineCount());
-        if(getOnlineCount()==0){
-            LoginModule.logout();
+    public void onClose(@PathParam("channel") Integer channel, Session session){
+        sessions.remove(session); //移除当前的sessions
+        log.error("有一连接关闭！当前在线人数为"+sessions.size());
+
+        if(sessions.size()==0){
+            channlAndlRealHandles.forEach((key, value)->{
+                RealPlayModule.stopRealPlay(value);
+            });
+        }else{
+            //所有通道村存的对饮的播放句柄
+            channlAndlRealHandles.forEach((key, value)->{
+                //当所有session里面的通道不在channlAndlRealHandles 说明已经退出
+                //直接断掉推流
+                if(!sessions.containsValue(key)){
+                    RealPlayModule.stopRealPlay(value);
+                }
+            });
+
         }
-//        String uuid = sid;
-//        DevicesModule devicesModule = DeviceListUtil.getDeviceModuleByDeviceId(device);
-//        if (sessions.containsKey(uuid)) {
-//            try{
-//                    long handle = OperationModule.findRealPlayInfoByDeviceIdAndChannelNum(device+channel);
-//                    RealPlayInfo realPlayInfo = OperationModule.findRealPlayInfo(handle);
-//                    if(realPlayInfo != null){
-//                        System.out.println(uuid + ":websocket 断开连接！");
-//                        boolean b = false;
-//                        if("1".equals(realPlayInfo.getType())){
-//                            b = devicesModule.stopPlay(new NetSDKLib.LLong(handle));
-//                        }else if("2".equals(realPlayInfo.getType())){
-//                            b = service.stopReport(new NetSDKLib.LLong(handle),realPlayInfo.getDeviceId());
-//                        }
-//                        if(b){
-//                            sessions.remove(uuid);
-//                            subOnlineCount();
-//                            OperationModule.removeRealPlayInfo(handle);
-//                        }
-//                }
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        }
+
+        subOnlineCount();
+
+
     }
 
     /**
@@ -163,9 +145,9 @@ public class WebSocketServerDh {
      * @param message 字符串类型的消息
      */
     public void sendAll(String message) {
-        for (Map.Entry<String, Session> session : sessions.entrySet()) {
+       /* for (Map.Entry<String, Session> session : sessions.entrySet()) {
             session.getValue().getAsyncRemote().sendText(message);
-        }
+        }*/
     }
 
     /**
@@ -174,9 +156,9 @@ public class WebSocketServerDh {
      * @param buffer
      */
     public void sendMessage(ByteBuffer buffer) {
-        for (Map.Entry<String, Session> session : sessions.entrySet()) {
+      /*  for (Map.Entry<String, Session> session : sessions.entrySet()) {
             session.getValue().getAsyncRemote().sendBinary(buffer);
-        }
+        }*/
     }
 
     /**
@@ -186,7 +168,7 @@ public class WebSocketServerDh {
      */
     public void forwardDataFlow(long realPlayHandler, ByteBuffer buffer) {
         //播放句柄无效
-        if (realPlayHandler == 0) {
+/*        if (realPlayHandler == 0) {
             log.error("loginHandler is invalid.please check.", this);
             return;
         }
@@ -208,7 +190,7 @@ public class WebSocketServerDh {
             }
         } else {
             log.error("session is null.please check.");
-        }
+        }*/
     }
 
     /**
@@ -219,7 +201,7 @@ public class WebSocketServerDh {
      */
     public void sendReportMessageToOne(long rePlayHandler, ByteBuffer buffer) {
         //登录句柄无效
-        if (rePlayHandler == 0) {
+/*        if (rePlayHandler == 0) {
             log.error("loginHandler is invalid.please check.", this);
             return;
         }
@@ -241,33 +223,53 @@ public class WebSocketServerDh {
             }
         } else {
 
-        }
+        }*/
     }
 
     /**
      * 实现服务器主动推送
      */
     public void sendMessageToOne(int nChannelID, ByteBuffer buffer) throws IOException {
+        for (Map.Entry<Session ,Integer> entry : sessions.entrySet()) {
 
-        //int nChannelID = lRealHandles.get(realPlayHandler);
-        for(String key : sessions.keySet()){
-            Session session = sessions.get(key);
-            if (session != null && session.isOpen()) { // 确保session不为null
-                synchronized (session) {
-                    if(key.substring(key.indexOf("-")+1,key.length()).equals(nChannelID+"")){
-                        try {
-                            session.getBasicRemote().sendBinary(buffer);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+            if(entry.getValue() == nChannelID){
+                try {
+                    /**
+                     * tomcat的原因,使用session.getAsyncRemote()会报Writing FULL WAITING error
+                     * 需要使用session.getBasicRemote()
+                     */
+
+                    System.out.println("发送数据"+buffer);
+                     entry.getKey().getBasicRemote().sendBinary(buffer);
+/*                    new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            try {
+                                entry.getKey().getBasicRemote().sendBinary(buffers);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
-                    }
+                    }).start();*/
+
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+
         }
+
+
+
+
+
+
     }
 
-    public static void sendBuffer(ByteBuffer buffer, int channel) {
+    public static void sendBuffer(ByteBuffer buffer, int channel,NetSDKLib.LLong lRealHandle) {
         WebSocketServerDh wsServerEndpoint = new WebSocketServerDh();
+        WebSocketServerDh.channlAndlRealHandles.put(channel, lRealHandle);
         /**
          * 发送流数据
          * 使用pBuffer.getByteBuffer(0,dwBufSize)得到的是一个指向native pointer的ByteBuffer对象,其数据存储在native,
@@ -284,13 +286,13 @@ public class WebSocketServerDh {
 
     public void sendMessageToAll(ByteBuffer buffer) {
 
-        for (Session session : sessions.values()) {
+/*        for (Session session : sessions.values()) {
             synchronized (session) {
                 try {
-                    /**
+                    *//**
                      * tomcat的原因,使用session.getAsyncRemote()会报Writing FULL WAITING error
                      * 需要使用session.getBasicRemote()
-                     */
+                     *//*
                     if (session != null && session.isOpen()) {
                         session.getBasicRemote().sendBinary(buffer);
                     }
@@ -298,7 +300,7 @@ public class WebSocketServerDh {
                     e.printStackTrace();
                 }
             }
-        }
+        }*/
     }
 
     /**
@@ -307,14 +309,14 @@ public class WebSocketServerDh {
      * @param realPlayHandler 预览句柄
      */
     public void closeSession(String realPlayHandler) {
-        try {
+  /*      try {
             Session session = sessions.get(realPlayHandler);
             if (session != null) {
                 session.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     /**
