a9ac39b0c504f84b12a6d2e333fa0e9fb25daa1a
ysy <172793208@qq.com>
2024-04-12 18:09:41 +0800

    升级JAN版本  修改gatewat websocket字节数

--- monitor/src/main/java/com/pd/monitor/netsdk/controller/RealPlayController.java
+++ monitor/src/main/java/com/pd/monitor/netsdk/controller/RealPlayController.java
@@ -8,6 +8,7 @@ import com.pd.monitor.netsdk.utils.ResultUtils;
 import com.pd.monitor.netsdk.websocketServer.WebSocketServerDh;
 import com.pd.server.config.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.*;
@@ -28,8 +29,7 @@ public class RealPlayController {
     // 设备信息
     private static NetSDKLib.NET_DEVICEINFO_Ex m_stDeviceInfo = new NetSDKLib.NET_DEVICEINFO_Ex();
 
-    // 登陆句柄
-    private static NetSDKLib.LLong m_hLoginHandle = new NetSDKLib.LLong(0);
+
 
     @PostMapping("/startPlay/{nChannelID}")
     public Result startPlay(@PathVariable  int nChannelID){
@@ -37,15 +37,29 @@ public class RealPlayController {
             // 初始化
             LoginModule.init(LoginModule.disConnect, LoginModule.haveReConnect);
             // 若未登录，先登录。
-            if (m_hLoginHandle.longValue() == 0){
+            if (LoginModule.m_hLoginHandle.longValue() == 0){
                 LoginModule.login("49.239.193.148", 43489, "admin", "admin@bht2023");
             }
             //
-            RealPlayModule.startRealPlay(LoginModule.m_hLoginHandle,nChannelID, 0, 5);
+           if(WebSocketServerDh.channlAndlRealHandles .size() == 0 ){
+                RealPlayModule.startRealPlay(LoginModule.m_hLoginHandle,nChannelID, 0, 5);
+            }else{
+                if(!WebSocketServerDh.channlAndlRealHandles.containsKey(nChannelID)){
+                    RealPlayModule.startRealPlay(LoginModule.m_hLoginHandle,nChannelID, 0, 5);
+                }else{
+                    if ( WebSocketServerDh.channlAndlRealHandles.get(nChannelID).longValue() == 0){
+                        RealPlayModule.startRealPlay(LoginModule.m_hLoginHandle,nChannelID, 0, 5);
+                    }
+                }
+
+            }
+
+
             return ResultUtils.getSuccessResult("登录成功");
         }catch (Exception e){
             return ResultUtils.getFailResult("登录失败");
         }
+
     }
 
     public static void main(String[] args){
@@ -53,7 +67,7 @@ public class RealPlayController {
             // 初始化
             LoginModule.init(LoginModule.disConnect, LoginModule.haveReConnect);
             // 若未登录，先登录。
-            if (m_hLoginHandle.longValue() == 0){
+            if (LoginModule.m_hLoginHandle.longValue() == 0){
                 LoginModule.login("49.239.193.148", 43489, "admin", "admin@bht2023");
             }
             RealPlayModule.startRealPlay(LoginModule.m_hLoginHandle,0, 0, 5);
