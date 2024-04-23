1377579c7231dea0ae36f217bbb3c680baac4018
xumin-0526 <18827512017@189.cn>
2024-04-23 15:30:22 +0800

    UDP接口

--- monitor/src/main/java/com/pd/monitor/controller/UdpService.java
+++ monitor/src/main/java/com/pd/monitor/controller/UdpService.java
@@ -1,12 +1,17 @@
 package com.pd.monitor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service("UdpService")
 public class UdpService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UdpService.class);
+
     public void handleMessage(byte[] message) {
         // 这里处理接收到的UDP数据
-        System.out.println("Received UDP message: " + new String(message));
+        LOG.error("Received UDP message: " + new String(message));
+        LOG.error("Received UDP size: " + message.length);
     }
 }
