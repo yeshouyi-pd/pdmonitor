33314bae390905ffd5ac485156f86e1be61a5953
xumin-0526 <18827512017@189.cn>
2024-10-29 09:57:58 +0800

    大屏样式修改

--- admin/src/components/date.vue
+++ admin/src/components/date.vue
@@ -21,32 +21,43 @@ methods: {
   dateInit(){
     let _this =this;
 
-    $("#"+_this.idValue).datepicker({
-      autoclose:true,
-      clearBtn:1,
-      todayHighlight : 1,
-      minView : 1,
-      forceParse : 0,
-    }).on('hide',(ev) => {
-      let d =   Tool.checkTime($("#"+_this.idValue).val()
+    $("#" + _this.idValue).datetimepicker({
+      format: 'YYYY-MM-DD',//显示格式
+      locale: moment.locale('zh-cn'),
+      icons: {
+        time: 'fa fa-clock-o',
+        date: 'fa fa-calendar',
+        up: 'fa fa-chevron-up',
+        down: 'fa fa-chevron-down',
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-arrows ',
+        clear: 'fa fa-trash',
+        close: 'fa fa-times'
+      }
+    }).on('dp.change', function (e) {
+      let d =   Tool.checkTime($("#"+_this.idValue).val().split(" ")[0]
           ,new Date().getFullYear()+"-"+(new Date().getMonth()+1)+"-"+new Date().getDate());//2020-12-03
       if(d){
-        //_this.cfDate($("#datepick").val());
         _this.$emit('methodName',$("#"+_this.idValue).val());
       }else {
         $("#"+_this.idValue).val("");
         Toast.warning("开始时间不能大于今天时间");
       }
-
-
     });
-
-
   }
 }
 }
 </script>
 <style scoped>
+/deep/.bootstrap-datetimepicker-widget {
+  color: #d0ccff !important; /* 修改日期选择框字体颜色 */
+  background-color: #121E32 !important;
+}
+/deep/.bootstrap-datetimepicker-widget table td span:hover{
+  color: #fff !important;
+  background:#00C3E6 !important;
+}
 .form-control{
   color: yellow !important; /* 修改日期选择框字体颜色 */
   background-color: rgb(19, 34, 94) !important;
