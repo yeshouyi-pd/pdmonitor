39bbd64f341d12b9e70c0a65e01ae54ee1f30708
xumin-0526 <18827512017@189.cn>
2025-03-25 13:02:15 +0800

    修改

--- admin/src/views/uav/uavFlyVideo.vue
+++ admin/src/views/uav/uavFlyVideo.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="widget-box">
       <div class="widget-header">
-        <h4 class="widget-title">图片管理查询</h4>
+        <h4 class="widget-title">视频管理查询</h4>
       </div>
       <div class="widget-body">
         <div class="widget-main">
