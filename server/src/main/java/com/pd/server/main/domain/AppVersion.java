f403b608484c844d32813b4bde6c720d4a1a7f91
风花 <172793208@qq.com>
2025-09-05 14:49:59 +0800

    添加业务基本信息表

--- server/src/main/java/com/pd/server/main/domain/AppVersion.java
+++ server/src/main/java/com/pd/server/main/domain/AppVersion.java
@@ -3,12 +3,42 @@ package com.pd.server.main.domain;
 public class AppVersion {
     private Long id;
 
+    private String version;
+
+    private String downloadurl;
+
+    private String description;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getDownloadurl() {
+        return downloadurl;
+    }
+
+    public void setDownloadurl(String downloadurl) {
+        this.downloadurl = downloadurl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
@@ -18,6 +48,9 @@ public class AppVersion {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
+        sb.append(", version=").append(version);
+        sb.append(", downloadurl=").append(downloadurl);
+        sb.append(", description=").append(description);
         sb.append("]");
         return sb.toString();
     }
